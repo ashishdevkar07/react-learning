5d42f779149915572954f13738ba218322b27ca4
Ashish Devkar <ashishdevkar761@gmail.com>
2026-06-08 10:31:10 +0530

    Day 1 React - setup, first component, JSX basics

---
+++ src/practice/MyfirstComponent.jsx
@@ -0,0 +1,10 @@
+function MyfirstComponent(){
+    return (
+        <div>
+            <h2>I am a Component</h2>
+            <p>My name is Ashish</p>
+            <p>I am learning react</p>
+        </div>
+    )
+}
+export default MyfirstComponent 
